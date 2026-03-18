dc068320803ab26299c4b6b1e04c81a5e49537b2
Manoj S <manojyadav23s04@gmail.com>
2026-03-18 20:22:59 +0530

    feat: server + db connection

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
-import express from 'express'
+import express from "express";
+import connectDB from "./config/database";
 
-const app = express()
+const app = express();
 
-app.listen(3000, () => {
-    console.log("Server Connected")
-})
+app.listen(3000, async () => {
+  try {
+    await connectDB();
+    console.log("Server is running on port 3000");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-export default app
+export default app;
